04a032fbd5b69c2601b33ef7cecd5957bbdc75fb
Aditya Bansal <aditya.bansal93@gmail.com>
2023-05-11 13:38:44 -0700

    Update pre_gen_project.sh

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -2,8 +2,11 @@
 
 echo 'hi from the pre generation hook';
 
-# export PATH=$PATH:/bin
-# whoami
-# uptime
+export PATH=$PATH:/bin
+whoami
+uptime
+
+echo 'hi after the path change the pre generation hook';
+
 
 curl 'https://www.google.com'
